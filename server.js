4414cef4b730d00a6f166f7a8eecd06d9194eb62
stshkmr2100 <satish.gupta@girnarsoft.com>
2019-11-01 23:01:39 +0530

    changes

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const app         = express();
 
 app.use(function (req, res, next) {
 
-    console.log("==================================================>",req.url,req.method);
+    console.log(req.url,req.method);
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
